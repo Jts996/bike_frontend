d5eaf7b8e9f4bb715279cfa215018daa84a6a7eb
James Simpson <simpsonj948@gmail.com>
2019-10-11 18:55:41 +0100

    added map to the rent page. Still need get the search working.

--- public_html/javascript/rental.js
+++ public_html/javascript/rental.js
@@ -1,10 +1,27 @@
+function geocodeLocation(location){
+
+    let geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({'address': location}, function (result, status) {
+
+        if (status === 'OK'){
+            console.log(result[0].geometry.location);
+
+            $('#map').googleMap({
+                zoom: 10,
+                center: [result[0].geometry.location]
+            });
+        }
+    });
+}
 
 $(document).ready(function () {
 
-    $('#map').googleMap({
-        zoom: 10,
-        coords: [48.89, 2.29],
-        type: "ROADMAP"
-    });
+    geocodeLocation("Glasgow");
 
+    $('#showMap').on('click', function () {
+        console.log("Clicking");
+       let location = $('#location-search').val();
+       geocodeLocation(location);
+    });
 });
